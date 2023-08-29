2db0598bf6b893de074b9c74f7d437076e65ec5f
Daphne Balane <balanedaphne20@gmail.com>
2023-08-29 22:15:29 +0800

    feat: added toggle and delete reducers

--- src/components/ToDoReducer.js
+++ src/components/ToDoReducer.js
@@ -6,11 +6,19 @@ const todoSlice = createSlice({
     reducers: {
         addTodo: (state, action) => {
             state.push(action.payload)
-            console.log(action.payload);
-        }
+        },
+        toggleTodo: (state, action) => {
+            const todo = state.find((item) => item.id === action.payload);
+            if (todo) {
+                todo.done = !todo.done;
+            }
+        },
+        deleteTodo: (state, action) => {
+            return state.filter((item) => item.id !== action.payload);
+        },
     }
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
