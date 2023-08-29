ae4e0900978eb59d51bd2d1fc4b074ea8ffaa51e
Daphne Balane <balanedaphne20@gmail.com>
2023-08-29 17:32:30 +0800

    feat: used useSelector in ToDoGroup

--- src/components/ToDoGroup.js
+++ src/components/ToDoGroup.js
@@ -1,4 +1,5 @@
 import { List, Typography } from 'antd';
+
 const ToDoGroup= (props) => {
     return (
         <div>
@@ -7,7 +8,7 @@ const ToDoGroup= (props) => {
                 dataSource={props.todoListItems}
                 renderItem={(item) => (
                 <List.Item>
-                <Typography.Text mark></Typography.Text> {item}
+                <Typography.Text mark>{item.done ? 'done' : 'not done'}</Typography.Text> {item.text}
                 </List.Item>
                 )}
             />          
