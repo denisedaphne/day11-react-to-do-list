b6574653e168984ed89e2b827146e334aab8b74a
Daphne Balane <balanedaphne20@gmail.com>
2023-08-29 22:15:58 +0800

    feat: added function for toggle and delete

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,6 +1,33 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleTodo, deleteTodo } from './ToDoReducer';
+import { Typography, Button } from 'antd';
+import { DeleteFilled } from '@ant-design/icons';
+import '../App.css';
+
 const ToDoItem = (props) => {
+    const dispatch = useDispatch();
+
+    const handleToggle = () => {
+        dispatch(toggleTodo(props.id));
+    };
+
+    const handleDelete = () => {
+        if (window.confirm('Are you sure you want to delete this todo?')) {
+            dispatch(deleteTodo(props.id));
+        }
+    };
+    
     return (
-        <div>{props.input}</div>
+        <div className='toDoList'>
+            <Typography.Text 
+                style={{ textDecoration: props.done ? 'line-through' : 'none'}}
+                onClick={handleToggle}
+                >
+                    {props.text}  
+            </Typography.Text> &nbsp;
+            <Button type="primary" danger icon={<DeleteFilled />} onClick={handleDelete}/>
+        </div>
     );
 }
 
