82f71de15e112b3fdbfccea7bed35a1a4c7dbda8
Daphne Balane <balanedaphne20@gmail.com>
2023-08-29 17:33:43 +0800

    feat: added ToDoReducer for slicing

---
+++ src/components/ToDoReducer.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+    name:'todos',
+    initialState: [],
+    reducers: {
+        addTodo: (state, action) => {
+            state.push(action.payload)
+            console.log(action.payload);
+        }
+    }
+});
+
+export const { addTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
