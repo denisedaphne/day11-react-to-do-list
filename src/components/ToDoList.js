86974694899fa0dd0df9518e238fb0ff9d41893e
Daphne Balane <balanedaphne20@gmail.com>
2023-08-31 00:00:27 +0800

    refactor: modified necessary codes

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import ToDoGroup from './ToDoGroup';
-import ToDoGenerator from './ToDoGenerator';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import * as todoApi from "../api/todoApi";
+import TodoGenerator from "./ToDoGenerator";
+import TodoGroup from "./ToDoGroup";
+import "./css/TodoList.css";
+import { resetTodoList } from "./ToDoReducer";
 
 const TodoList = () => {
-    const todosList = useSelector((state) => 
-        state.todos
-    )
+    const dispatch = useDispatch();
+    useEffect(()=> {
+        const fetchData = async () => {
+            const response = await todoApi.getTodoTasks();
+            dispatch(resetTodoList(response.data));
+        }
+        fetchData();
+    },[])
 
-  return (
-    <>
-      <ToDoGroup todoListItems={todosList} />
-      <ToDoGenerator />
-    </>
-  );
+    return (
+        <div className="todoListContainer">
+            <h2 className="todoListTitle"> Todo List</h2>
+            <div className="todoListComponents">
+                <TodoGroup />
+                <TodoGenerator />
+            </div>
+        </div>
+    );
 };
-
 
 export default TodoList;
