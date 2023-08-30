24f8b108a85a0505bf234a35df6241664298dd0d
Daphne Balane <balanedaphne20@gmail.com>
2023-08-31 00:00:36 +0800

    refactor: modified necessary codes

--- src/components/ToDoReducer.js
+++ src/components/ToDoReducer.js
@@ -1,24 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const todoSlice = createSlice({
-    name:'todos',
-    initialState: [],
-    reducers: {
-        addTodo: (state, action) => {
-            state.push(action.payload)
-        },
-        toggleTodo: (state, action) => {
-            const todo = state.find((item) => item.id === action.payload);
-            if (todo) {
-                todo.done = !todo.done;
-            }
-        },
-        deleteTodo: (state, action) => {
-            return state.filter((item) => item.id !== action.payload);
-        },
-    }
+   name: "todo",
+   initialState: {
+      todoList: [],
+   },
+   reducers: {
+      resetTodoList: (state, action) => {
+         state.todoList = action.payload;
+      },
+
+   }
 });
 
-export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
-
+export const { resetTodoList } = todoSlice.actions;
 export default todoSlice.reducer;
